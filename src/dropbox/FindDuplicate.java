05c5327298fbc7e96222e620bcb62667bb3a8630
Bo Zhou <bo.zhou@wandisco.com>
2019-07-10 15:43:37 -0700

    Add more companies

---
+++ src/dropbox/FindDuplicate.java
@@ -0,0 +1,7 @@
+package dropbox;
+
+public class FindDuplicate {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ src/dropbox/GameOfLife.java
@@ -0,0 +1,9 @@
+package dropbox;
+
+public class GameOfLife {
+
+
+  public static void main(String[] args) {
+    System.out.println("Output");
+  }
+}
