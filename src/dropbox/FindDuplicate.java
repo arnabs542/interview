37e36658bd9802fb2dfb1eab240bd621585a5c3e
Bo Zhou <bo.zhou@wandisco.com>
2019-07-23 12:01:09 -0700

    Add LinkedIn questions

--- src/dropbox/FindDuplicate.java
+++ src/dropbox/FindDuplicate.java
@@ -2,6 +2,6 @@ package dropbox;
 
 public class FindDuplicate {
   public static void main(String[] args) {
-
+    System.out.println("Test");
   }
 }
--- src/dropbox/GameOfLife.java
+++ src/dropbox/GameOfLife.java
@@ -2,7 +2,6 @@ package dropbox;
 
 public class GameOfLife {
 
-
   public static void main(String[] args) {
     System.out.println("Output");
   }
