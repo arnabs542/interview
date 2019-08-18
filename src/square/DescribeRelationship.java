97b61f7f8377f201e31be699f23596ea0029e61d
Bo Zhou <bo.zhou@wandisco.com>
2019-08-18 14:03:31 -0700

    Add more square questions

---
+++ src/square/DescribeRelationship.java
@@ -0,0 +1,42 @@
+package square;
+
+/**
+ * a. Parsing: Given a string like "Chris,Tom,Anne\nJerry,Tim,Daphne"
+ * Print out the string like
+ * Chris is the child of Tom and Anne
+ * Jerry is the child of Tim and Daphne
+ *
+ * b. Find out the child
+ *
+ * Same function, describeRelationship(name1, name2)
+ *
+ * Output either
+ * "child"
+ * "unknown"
+ *
+ * c. Find out the parent
+ *
+ * Same function, describeRelationship(name1, name2)
+ *
+ * Output either
+ * "child"
+ * "unknown"
+ * "parent"
+ *
+ * d. Find out ancestor
+ * Output
+ * "child"
+ * "unknown"
+ * "parent"
+ * “ancestor”
+ *
+ * e. Find out offspring
+ * Output
+ * "child"
+ * "unknown"
+ * "p‍‌‌‍‍‍‌‌‌‌‌‌‍‍arent"
+ * “ancestor”
+ * "offspring"
+ */
+public class DescribeRelationship {
+}
---
+++ src/square/ValidPlaylist.java
@@ -0,0 +1,25 @@
+package square;
+
+import java.util.List;
+
+/**
+ * 有一个class叫Song, 里面的key是一个int（0到11） 。value是歌名。现在给你一个playlist, 判断这个playlist是不是valid的。
+ * Follow的rule是他们的key是adjacent或者是它本身。但是这个adjacent是针对一个环。
+ * 比如 0, 1, 2, 3是valid的。10，11，0，1也是valid的。 1， 0 ，0， 11是valid的。但是10，1，2不是valid的。
+ * follow up:
+ * 现在给你一个valid的playlist和一首‍‌‌‍‍‍‌‌‌‌‌‌‍‍歌， 返回所有可以插入这首歌还能保证playlist是valid的位置
+ */
+public class ValidPlaylist {
+  class Song {
+    int key;
+    String value;
+    public Song(int key, String value) {
+      this.key = key;
+      this.value = value;
+    }
+  }
+
+  boolean validPlaylist(List<Song> playlist) {
+    return true;
+  }
+}
